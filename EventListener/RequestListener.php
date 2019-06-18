520958ec78bfe34d35a19724cce092cb7e39500e
ruano-a <aurelien.ruano@epitech.eu>
2019-06-19 00:03:01 +0200

    update

--- EventListener/RequestListener.php
+++ EventListener/RequestListener.php
@@ -3,43 +3,83 @@
 namespace ruano_a\AccessLimiterBundle\EventListener;
 
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 class RequestListener
 {
     private $templating;
-    const VIEW_PATH = 'gate.html.twig';
+    const VIEW_PATH = '@AccessLimiter/gate.html.twig';
+    const SESSION_VAR = 'AccessLimiter_ALLOWED';
 
-	public function __construct(EngineInterface $templating)
-	{
+    public function __construct(Environment $templating)
+    {
         $this->templating = $templating;
-	}
-	
-	protected function isAllowed()
-	{
-        return (false);
-	}
+    }
+
+    protected function isAllowed()
+    {
+       $session = null;
+
+       //return ($session->get(self::SESSION_VAR));
+       return (false);
+    }
+
+    protected function setAllowed()
+    {
+        $session = truc;
+        $session->set(self::SESSION_VAR, true);
+    }
 
     protected function getResponse(string $errorMessage = null)
     {
-        return $this->templating->renderResponse(self::VIEW_PATH, ['error' -> $errorMessage]);
+        return new Response($this->templating->render(self::VIEW_PATH, ['error' => $errorMessage]));
     }
 
     protected function checkPassword(string $password)
     {
+        $passwords = STUFF;
+
+        return (in_array($password, $passwords));
         return (false);
     }
 
+    protected function containsFormData($request)
+    {
+        // todo, see the dump of the request
+        return (false);
+    }
+
+    protected function isActive()
+    {
+        //return ($activeParam);
+        return (true);
+    }
+
+    // for now, let's say it's only by password
     protected function handleNotAllowedRequest(GetResponseEvent $event)
     {
         $request = $event->getRequest();
-        dump($request);
-        exit();
+        if ($this->containsFormData($request))
+        {
+            if ($this->checkPassword($request->getPassword()))
+            {
+                $this->setAllowed();
+            }
+            else
+            {
+                $event->setResponse($this->getResponse('Wrong password'));
+            }
+        }
+        else
+        {
+            $event->setResponse($this->getResponse());
+        }
     }
-
+    
     public function onKernelRequest(GetResponseEvent $event)
     {
-        if (!$event->isMasterRequest() || $this->isAllowed()) {
+        if (!$this->isActive() || !$event->isMasterRequest() || $this->isAllowed()) {
             // don't do anything if it's not the master request
             return;
         }
