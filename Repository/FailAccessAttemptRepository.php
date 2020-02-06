465a348e842019c15f1206f9873c2c4b621612e8
Aurelien Ruano <aurelien.ruano@epitech.eu>
2020-02-06 21:21:11 +0100

    change RegistryInterface to ManagerRegistry, for recent versions of doctrine

--- Repository/FailAccessAttemptRepository.php
+++ Repository/FailAccessAttemptRepository.php
@@ -4,18 +4,19 @@ namespace ruano_a\AccessLimiterBundle\Repository;
 
 use ruano_a\AccessLimiterBundle\Entity\FailAccessAttempt;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 class FailAccessAttemptRepository extends ServiceEntityRepository
 {    
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, FailAccessAttempt::class);
     }
 
     public function getFailAccessAttemptsFrom($ip, $beginDate)
     {
-        return $this->createQueryBuilder('f')        ->where('f.ip = :ip')
+        return $this->createQueryBuilder('f')
+        ->where('f.ip = :ip')
         ->andWhere('f.lastFailDate > :beginDate')
         ->setParameter('ip', $ip)
         ->setParameter('beginDate', $beginDate)
